905f5ce16175c58efdaa14e34bd030e94cd3f277
yury-chu <tesewar@mail.ru>
2016-06-04 16:14:52 +0300

    добавление таблицы многие ко многим

--- install/create_db.sql
+++ install/create_db.sql
@@ -3,19 +3,42 @@ CREATE DATABASE catalog_site;
 USE catalog_site;
 
 CREATE TABLE category (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT,
   name VARCHAR(50),
   short_name TEXT,
   full_name TEXT,
-  active INT
-);
+  active INT,
+
+  PRIMARY KEY (id)
+
+) ENGINE=INNODB;
 
 CREATE TABLE goods (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT,
   name VARCHAR(50),
   short_name TEXT,
   full_name TEXT,
   active INT,
   in_stock INT,
-  can_be_ordered INT
-);
+  can_be_ordered INT,
+
+  PRIMARY KEY (id)
+
+) ENGINE = INNODB;
+
+CREATE TABLE category_goods (
+  id INT AUTO_INCREMENT,
+  category_id INT NOT NULL,
+  goods_id INT NOT NULL,
+
+  PRIMARY KEY (id),
+
+  FOREIGN KEY (category_id)
+    REFERENCES category(id)
+    ON DELETE CASCADE,
+
+  FOREIGN KEY (goods_id)
+    REFERENCES goods(id)
+    ON DELETE CASCADE
+
+) ENGINE=INNODB;
